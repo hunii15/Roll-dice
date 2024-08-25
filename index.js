3008242ea8a3f76d9873c6ad954d9817821fcb2b
Jihun Jung <iooikj4809@gmail.com>
2024-08-25 15:30:59 +1200

    updated js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function rollDice(sides) { //Roll function
     return Math.floor(Math.random() * sides) + 1; // +1 = Number starting from 1
 }
 
-document.getElementById('rollButton').addEventListener('click', function() { //Function names
-    const diceType = document.getElementById('diceType').value; //D6 or D10
-    const result = rollDice(diceType); //Roll button
-    document.getElementById('result').textContent = `Answer: ${result}`; //Answer
+document.getElementById('Button').addEventListener('click', function() { //Roll Function 
+    const diceType = document.getElementById('Type').value; //D6 or D10
+    const result = rollDice(diceType); //Roll + Answer
+    document.getElementById('result').textContent = `Answer: ${result}`; //Answer:
 });
